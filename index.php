0b7cb92f9063791ee1da52609ad79fb003cb5abe
RizkiAkhmal <155047782+RizkiAkhmal@users.noreply.github.com>
2024-05-20 11:29:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,11 @@ require_once "koneksi.php";
             <th>Judul</th>
             <th>Pengarang</th>
             <th>Tahun Terbit</th>
-            <th>Aksi</th>
+            <th>Nama Peminjam</th>
+            <th>Tanggal peminjaman</th>
+            <th>Tanggal Pengembalian</th>
+            <th>delete Book</th>
+            <th>Edit books</th>
         </tr>
         <?php
         $books = json_decode(file_get_contents('books.json'), true);
@@ -24,7 +28,11 @@ require_once "koneksi.php";
             echo "<td>{$book['judul']}</td>";
             echo "<td>{$book['pengarang']}</td>";
             echo "<td>{$book['tahun']}</td>";
+            echo "<td>{$book['peminjam']}</td>";
+            echo "<td>{$book['tanggal_peminjaman']}</td>";
+            echo "<td>{$book['tanggal_pengembalian']}</td>";
             echo "<td><a href='deleteBook.php?id={$index}'>Hapus</a></td>";
+            echo "<td><a href='addBook.php?edit={$index}'>Edit</a></td>";
             echo "</tr>";
         }
         ?>
